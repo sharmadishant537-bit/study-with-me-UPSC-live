c5b8fd623eb417245864380b4408a54a1a423bb9
sharmadishant537-bit <sharmadishant537@gmail.com>
2026-07-01 10:07:54 +0530

    Update script.js 2

--- script.js
+++ script.js
@@ -32,3 +32,22 @@ function updateWebsite() {
 
 setInterval(updateWebsite, 1000);
 updateWebsite();
+const status = document.getElementById("status");
+const joinBtn = document.getElementById("joinBtn");
+
+function updateStatus() {
+  const hour = new Date().getHours();
+
+  if (hour >= 23 || hour < 11) {
+    status.innerHTML = "🔴 LIVE NOW";
+    status.style.background = "#ff2d55";
+    joinBtn.style.display = "inline-block";
+  } else {
+    status.innerHTML = "⚫ OFF TIME";
+    status.style.background = "#555";
+    joinBtn.style.display = "none";
+  }
+}
+
+updateStatus();
+setInterval(updateStatus, 60000);
